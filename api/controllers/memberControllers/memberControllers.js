4eaec1d69e69a7426e1ead963736dce5eb1b9e3e
ttomasbenitez <tomivenitez.14@gmail.com>
2024-05-15 16:20:05 -0300

    Miembro puede salir de grupo.

--- api/controllers/memberControllers/memberControllers.js
+++ api/controllers/memberControllers/memberControllers.js
@@ -60,26 +60,45 @@ const addMember = async (req, res) => {
 };
 
 const deleteMember = async (req, res) => {
-    // try {
-    //     const groupId = req.params.group_id;
-
-    //     const query = {
-    //         text: `
-    //         SELECT g.id, g.name, g.description
-    //         FROM groups g
-    //         INNER JOIN members m ON g.id = m.group_id
-    //         WHERE m.username = $1
-    //         AND g.id = $2`,
-    //         values: [username, groupId]
-    //     };
-
-    //     const result = await client.query(query);
-
-    //     res.json({ group: result.rows });
-    // } catch (error) {
-    //     console.error("Error al obtener los datos:", error);
-    //     res.status(500).send("Error en el servidor");
-    // }
+    //try {
+    //    const groupId = req.params.group_id
+    //    const query = {
+    //        text: `
+    //        SELECT g.id, g.name, g.description
+    //        FROM groups g
+    //        INNER JOIN members m ON g.id = m.group_id
+    //        WHERE m.username = $1
+    //        AND g.id = $2`,
+    //        values: [username, groupId]
+    //    }
+    //    const result = await client.query(query)
+    //    res.json({ group: result.rows });
+    //} catch (error) {
+    //    console.error("Error al obtener los datos:", error);
+    //    res.status(500).send("Error en el servidor");
+    //}
 };
 
-module.exports = { getMembers, addMember, deleteMember };
+
+const exitGroup = async (req, res) => {
+    try {
+        const groupId = req.params.group_id;
+        const username = req.user.username; 
+
+        let result = await client.query('SELECT * FROM members WHERE username = $1 AND group_id = $2', [username, groupId]);
+
+        if (result.rows.length === 0) {
+            console.error("User is not a member of the group.");
+            return res.status(404).json({ message: 'User is not a member of the group.' });
+        }
+
+        result = await client.query('DELETE FROM members WHERE username = $1 AND group_id = $2', [username, groupId]);
+
+        res.status(200).json({ message: 'User successfully exited the group.' });
+    } catch (error) {
+        console.error("Error exiting group:", error);
+        res.status(500).send("Error exiting group.");
+    }
+};
+
+module.exports = { getMembers, addMember, deleteMember, exitGroup };
