f323d4fde3ac8c296712fb02c1fbd14eaaf41c81
agustincolman0310 <agustincolman0310@gmail.com>
2024-05-19 19:48:14 -0300

    integracion back con front de creacion de grupos

--- web/src/components/UsersList.js
+++ web/src/components/UsersList.js
@@ -1,83 +1,94 @@
-import React, { useState, useEffect } from 'react';
-import Button from '../components/Button'
-
+import React, { useState, useEffect } from "react";
+import Button from "../components/Button";
+import { useParams } from "react-router-dom";
 
 const UserList = () => {
   const [users, setUsers] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
+  const params = useParams();
+  console.log("Este es el parametro: " + params.id)
 
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await fetch('http://localhost:3001/users');
+        const response = await fetch("http://localhost:3001/users");
         if (!response.ok) {
-          throw new Error('Failed to fetch users');
+          throw new Error("Failed to fetch users");
         }
-        
+
         const data = await response.json();
         setUsers(data.users);
-
       } catch (error) {
-        console.error('Error fetching users:', error);
+        console.error("Error fetching users:", error);
       }
     };
 
     fetchUsers();
   }, []);
+  
+  const addMember = async (member) => {
+    try {
+        await fetch(`http://localhost:3001/groups/${params.id}/members`, {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.getItem('jwt-token')
+            },
+            body: JSON.stringify({
+                username: member
+            }),
+        })
+    .then( res => res)
+    
 
-  const filteredUsers = users.filter(user =>
-    user.name.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+    //if (!response.ok) {
+      //      alert('Aca hay un error');
+        //    const errorData = await response.json();
+          //  throw new Error(errorData.message);
+        //}
+    
+        //window.location.href = "/login";
+    } catch (error) {
+        console.error('Error creando el grupoooo:', error.message);
+        alert('No se pudo crear el grupo: ' + error.message);
+    }
+};
 
   return (
 
-  
-    <body>
-        <div class="container text-center mt-4">
-            <h2>Lista de usuarios</h2>
-        </div>
-        
-        <div class="container text-center">
-            <div class="input-group mb-3">
-              <input type="text" class="form-control" placeholder="Nombre usuario" aria-label="Nombre usuario" value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)}></input>
-            </div>
+    (
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Apellido</th>
+            <th scope="col">Nombre de usuario</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user1, index) => (
+            <tr>
+              <th scope="row">{index}</th>
+              <td>{user1.name}</td>
+              <td>{user1.lastname}</td>
+              <td>{user1.username}</td>
+              <td>
+                <button
+                  type="button"
+                  class="btn btn-primary"
+                  //onChange={() => setUsername(user1.username)}
+                  onClick={()=>addMember(user1.username)}
 
-            <div class="row align-items-start">
-                <div class="col border-bottom">
-                    Nombre
-                </div>
-                <div class="col border-bottom">
-                  Apellido
-                </div>
-                <div class="col border-bottom">
-                  Username
-                </div>
-                <div class="col border-bottom">
-                  Seleccionar
-                </div>
-
-                <hr />
-                
-                {filteredUsers.map((user, index) => (
-                    <div className="row" key={index}>
-                        <div className="col border-left border-bottom">{user.name}</div>
-                        <div className="col border-bottom">{user.lastname}</div>
-                        <div className="col border-bottom">{user.username}</div>
-                        <div className="col border-bottom"><input type="checkbox"/></div>
-                    </div>
-                ))}
-            </div>
-
-        <div className="container text-right mt-5">
-          <Button text="Crear grupo"/>
-        </div>
-              
-        </div>
-    </body>
-     
-   
-    
+                  
+                >
+                  Añadir
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    )
   );
 };
 
--- web/src/routes/Dashboard.js
+++ web/src/routes/Dashboard.js
@@ -1,42 +1,98 @@
 import React from "react";
-import './Home.css'; 
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import "./Home.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+
 
 function Dashboard() {
+  const [groups, setGroups] = useState([]);
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    const fetchGroups = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/groups", {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: localStorage.getItem("jwt-token"),
+          },
+        });
+        if (!response.ok) {
+          throw new Error("Failed to fetch users");
+        }
+
+        const data = await response.json();
+        setGroups(data.Groups);
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+
+    fetchGroups();
+  }, []);
+
   return (
-    <div className="Home"> {}
-        <div>
-            <Header href='/dashboard'/>
-        </div>
-        <body className="container mt-5">
+    <div className="Home">
+      {" "}
+      {}
+      <div>
+        <Header href="/dashboard" />
+      </div>
+      <body className="container mt-5">
+        <h2 className="body text-center">Welcome to BillBuddy</h2>
+        <h3 className="body text-center mt-5">Mis grupos</h3>
 
-          <h2 className="body text-center">Welcome to BillBuddy</h2>
-          <h3 className="body text-center mt-5">Mis grupos</h3>
+        <div class="container text-left">
+          <table class="table table-dark">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Descripción</th>
+                <th scope="col">Acción</th>
+              </tr>
+            </thead>
+            <tbody>
+              {groups.map((group, index) => (
+                <tr>
+                  <th scope="row">{index}</th>
+                  <td>{group.name}</td>
+                  <td>{group.description}</td>
+                  <td>
+                    <div
+                      class="btn-group"
+                      role="group"
+                      aria-label="Basic example"
+                    >
+                      <div class="btn-group">
+                      <button class="btn btn-primary">
+                          Editar
+                        </button>
+                        <button onClick={() =>  navigate(`add/${group.id}`)} class="btn btn-warning">
+                          Añadir participantes
+                        </button>
+                      </div>
+                      
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
 
-          <div class="container text-left">
-
-            <div class="list-group mt-5">
-              <a href="#" class="list-group-item list-group-item-action">Familia</a>
-              <a href="#" class="list-group-item list-group-item-action">Vacaciones</a>
-              <a href="#" class="list-group-item list-group-item-action">Salidas</a>
-              <a href="#" class="list-group-item list-group-item-action">F5</a>
-            </div>
-
-            <div className="d-flex justify-content-end mt-5">
-              <a href="/group/create" class="btn btn-primary">Crear grupo</a>
-            </div>
-
+          <div className="d-flex justify-content-end mt-5">
+            <a href="/group/create" class="btn btn-primary">
+              Crear grupo
+            </a>
           </div>
-           
-        </body>
-
-        <div>
-            <Footer/>
         </div>
+      </body>
+   
     </div>
   );
 }
-
 
 export default Dashboard;
